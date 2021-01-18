f2e033305efef816d6a2f2378a7cc59e35dee492
Emerald <ejzhang2004@gmail.com>
2021-01-17 23:53:31 -0600

    add column

---
+++ app/src/main/java/com/example/idea/User.java
@@ -0,0 +1,37 @@
+package com.example.idea;
+
+public class User {
+
+    private int id;
+    private String full_name;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return full_name;
+    }
+    public void setName(String full_name) {
+        this.full_name = full_name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
+
+
